9dd4f10ab8f1fd8c02a9553639eb8403b29433fa
koheiinaba <kouhei0419.s.s@gmail.com>
2020-04-16 07:13:45 +0000

    routes add

--- app/controllers/costomers/costomers_controller.rb
+++ app/controllers/costomers/costomers_controller.rb
@@ -7,4 +7,9 @@ class Costomers::CostomersController < ApplicationController
 
   def new
   end
+
+  private
+  def costomer_params
+    params.require(:costomer).permit(:first_name, :last_name, :first_kana_name, :last_kana_name, :postcode, :address, :phone, :status,)
+  end
 end
