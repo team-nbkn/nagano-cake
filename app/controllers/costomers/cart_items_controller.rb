3116be4ce00b8cafe66d10940156d413b48867a4
hyuna-hub <hombo.yuna.04510@gmail.com>
2020-04-19 13:40:13 +0000

    カート機能更新

--- app/controllers/costomers/cart_items_controller.rb
+++ app/controllers/costomers/cart_items_controller.rb
@@ -2,17 +2,42 @@ class Costomers::CartItemsController < ApplicationController
   layout 'costomers'
 
   def show
+    @cart_items = CartItem.where(costomer_id: current_costomer.id)
+    @products = Product.all
   end
 
   def create
-  	@cart_item = CartItem.new(cart_item_params)
+  	@cart_item = CartItem.new(cart_item)
   	if @cart_item.save
   		redirect_to costomers_cart_item_path(@cart_item.id)
   	end
   end
 
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+       redirect_to costomers_cart_item_path
+    end
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to costomers_cart_item_path(@cart_item.id)
+  end
+
+  def all_destroy
+    @cart_items = CartItem.all
+    @cart_items.destroy
+    redirect_to costomers_cart_item_path(@cart_item.id)
+  end
+
   private
     def cart_item_params
+      params.require(:cart_item).permit(:costomer_id, :product_id, :order_quantity)
+    end
+
+    def cart_item
     	{}.tap do |cart_item|
     		cart_item["product_id"] = params["product_id"]
     		cart_item["order_quantity"] = params["order_quantity"]
