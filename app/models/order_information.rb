1d0bfd7c7350a7e54ae78864b04378626eeaf90a
nakamura999 <masahiro9ama9@gmail.com>
2020-04-20 02:03:52 +0000

    adminmodel

--- app/models/order_information.rb
+++ app/models/order_information.rb
@@ -8,4 +8,7 @@ class OrderInformation < ApplicationRecord
   validates :address, presence: true
   validates :postcode, presence: true
   validates :name, presence: true
+
+  enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済: 4 }
+
 end
