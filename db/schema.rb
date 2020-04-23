6c7114ac2ae63465175d19c6d833b5aed746249b
hyuna-hub <hombo.yuna.04510@gmail.com>
2020-04-23 09:22:53 +0000

    注文情報入力確認

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_085526) do
+ActiveRecord::Schema.define(version: 2020_04_23_091140) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2020_04_19_085526) do
   create_table "order_products", force: :cascade do |t|
     t.integer "price"
     t.integer "quantity"
-    t.integer "status"
+    t.integer "status", default: 0
     t.integer "product_id"
     t.integer "order_information_id"
     t.datetime "created_at", null: false
